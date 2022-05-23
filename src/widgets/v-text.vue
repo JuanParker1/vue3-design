7fbe844ae0d7484884cd733795a81b77098df9a6
wangyuan389 <2814579279@qq.com>
2022-05-23 17:37:13 +0800

    feat：继续开发text物料

--- src/widgets/v-text.vue
+++ src/widgets/v-text.vue
@@ -1,22 +1,24 @@
 
 <template>
-  <div class="text">
-    <div
+  <div class="text" @dblclick="enterEdit">
+    <span
+      :id="textId"
       class="text-content"
       ref="textRef"
       tabindex="0"
       :contenteditable="isEidt"
       v-html="props.item.value"
-      @dblclick="enterEdit"
       @mousedown="handleMousedown"
       @keydown="handleKeydown"
-      @blur="handleBlur"
-    ></div>
+      @blur="exitEdit"
+      @input="changeVal"
+    ></span>
   </div>
 </template>
 
 <script setup lang='ts'>
-import { ref } from "vue";
+import { ref, computed, watch } from "vue";
+import _ from "lodash";
 
 const props = defineProps({
   item: {
@@ -24,9 +26,33 @@ const props = defineProps({
   },
 });
 
+let oldHeiget = _.cloneDeep(props.item.style.height);
 let isEidt = ref(false);
 const textRef = ref<HTMLElement | null>(null);
 
+const textId = computed(() => {
+  return "text-" + props.item.id;
+});
+
+watch(
+  props.item.style,
+  (curVal, preVal) => {
+    let scale = curVal.height / oldHeiget;
+    console.log('curVal.height', curVal.height);
+    console.log('oldHeiget', oldHeiget);
+    console.log('scale', scale);
+    console.log("preVal", JSON.stringify(preVal));
+    // 不在编译状态
+    if (!isEidt.value) {
+      props.item.style.fontSize = props.item.style.fontSize * scale;
+      console.log("props.item.style.fontSize", props.item.style.fontSize);
+    }
+    oldHeiget = curVal.height;
+  },
+  { deep: true }
+);
+
+// 进入编辑模式
 function enterEdit(e: any) {
   isEidt.value = true;
   selectAll();
@@ -34,12 +60,28 @@ function enterEdit(e: any) {
   textRef.value && textRef.value.focus();
 }
 
+// 失去焦点模式，退出编辑
+function exitEdit(e: any) {
+  console.log("失去焦点，退出编辑");
+  props.item.value = e.target.innerHTML || "&nbsp;";
+  isEidt.value = false;
+}
+
+// 修改text
+function changeVal(e: any) {
+  // props.item.value = e.target.innerHTML || "&nbsp;";
+  console.log("props.item.value", e.target.innerHTML);
+  // 获取当前高度,更新给物料
+  if (textRef.value) {
+    props.item.style.height = textRef.value.getBoundingClientRect().height;
+  }
+}
+
 // 文本全选
 function selectAll() {
   const selection: any = window.getSelection();
   const range = document.createRange();
   range.selectNodeContents(textRef.value as any);
-  console.log("textRef.value", textRef.value);
   selection.removeAllRanges();
   selection.addRange(range);
 }
@@ -47,36 +89,28 @@ function selectAll() {
 function handleMousedown(e: any) {
   if (isEidt.value) {
     e.stopPropagation();
-    console.log("handleMousedown");
   }
 }
 
+// 特殊按键操作
 function handleKeydown(e: any) {
-  console.log("handleKeydown");
-  console.log("e.keyCod", e.keyCode);
+  // console.log("handleKeydown");
+  // console.log("e.keyCod", e.keyCode);
   if ([8, 46].includes(e.keyCode)) {
     // deleteKey
     e.stopPropagation();
   }
-}
-
-// 失去焦点
-function handleBlur(e: any) {
-  console.log("失去焦点");
-  props.item.value = e.target.innerHTML || "&nbsp;";
-  isEidt.value = false;
 }
 </script>
 
 <style lang="scss" scoped>
 .text {
   width: 100%;
-  height: 100%;
-  padding: 5px;
   .text-content {
-    width: 100%;
-    height: 100%;
+    display: inline-block;
     outline: none;
+    line-height: 1.8;
+    word-break: break-all;
   }
 }
 </style>
